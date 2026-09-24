ceb8e86dc88b5ab10f216097ce1ffdbdb4efa686
Dedi Mohammad Kurnia <kurniawan.noperfect@gmail.com>
2026-09-25 05:42:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-﻿console.log('Halo, ini uji coba pipeline Jenkins > GitHub > ArgoCD');
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Halo bang Dedi, ini uji coba pipeline Jenkins > GitHub > ArgoCD berjalan sukses di Kubernetes!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server melek dan standby di port ${port}`);
+});
